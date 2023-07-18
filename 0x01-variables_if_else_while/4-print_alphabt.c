adc7262fd6f094d777c595d3e2abbc38e80beeb4
souhayl_dev <souhayl.ela@gmail.com>
2023-07-17 17:02:49 -0700

    c coding , if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		if (l != 'e' && l != 'q')
+		{
+			putchar(l);
+		}
+		l++;
+	}
+
+	putchar('\n');
+	return (0);
+}
